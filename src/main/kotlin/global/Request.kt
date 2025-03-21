95b852d391dfcb56e877f5e0e9c8f94944397281
chacha86 <taejincha0619@gmail.com>
2025-03-21 09:38:08 +0900

    request refactoring

--- src/main/kotlin/global/Request.kt
+++ src/main/kotlin/global/Request.kt
@@ -3,23 +3,20 @@ package com.think.global
 class Request(
     val input: String
 ) {
-    var actionName: String = ""
-    val paramMap = mutableMapOf<String, String>()
+    var actionName: String
+    val paramMap: Map<String, String>
 
     init {
-        val inputBits = input.split("?", limit=2)
+        val inputBits = input.split("?", limit = 2)
         actionName = inputBits[0]
-
-        if(inputBits.size == 2) {
-            val params = inputBits[1].split("&")
-
-            params.forEach { param ->
-                val paramBits = param.split("=", limit=2)
-
-                if(paramBits.size == 2) {
-                    paramMap[paramBits[0]] = paramBits[1]
-                }
+        paramMap = if (inputBits.size == 2) {
+            inputBits[1].split("&").associate {
+                val bits = it.split("=", limit = 2)
+                bits[0] to bits[1]
             }
+                .toMap()
+        } else {
+            emptyMap()
         }
     }
 
